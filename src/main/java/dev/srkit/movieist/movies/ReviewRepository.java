d862a74f16fad2449f03262490a1432205195071
sadasrkgithub18 <shivvareddy2233@gmail.com>
2024-03-16 21:55:18 -0500

    Complete Project Code

---
+++ src/main/java/dev/srkit/movieist/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.srkit.movieist.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
